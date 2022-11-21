0ef99ecada711b80a3a3276b9c2f8654b3716c28
Geovanny Reyes <geovanny.reyes@correounivalle.edu.co>
2022-11-21 11:57:53 -0500

    User API

--- members/views.py
+++ members/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = UserSerializer
